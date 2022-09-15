8766ca5004f4c9c021a58bca020c74fee35634e7
Ivie Osoiye <ivieosoiye@gmail.com>
2022-09-16 00:09:08 +0100

    created the room class

---
+++ src/model/Room.java
@@ -0,0 +1,36 @@
+package model;
+
+public abstract class Room implements IRoom{
+    private final String roomNumber;
+    private final Double price;
+    private final RoomType enumeration;
+
+    public Room(String roomNumber, Double price, RoomType enumeration) {
+        this.roomNumber = roomNumber;
+        this.price = price;
+        this.enumeration = enumeration;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public Double getRoomPrice() {
+        return price;
+    }
+
+    public RoomType getRoomType() {
+        return enumeration;
+    }
+
+    public abstract boolean isFree();
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomNumber='" + roomNumber + '\'' +
+                ", price=" + price +
+                ", enumeration=" + enumeration +
+                '}';
+    }
+}
