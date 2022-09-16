40b2f7ecf297c87a339d918ecc2ed6aa50873289
Ivie Osoiye <ivieosoiye@gmail.com>
2022-09-16 01:34:30 +0100

    added email validation using regex

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,14 +1,24 @@
 package model;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Customer {
     private String firstName;
     private String lastName;
     private String email;
 
+    private static final String emailPattern = "^(.+)@(.+).(.+)$";
+    private static final Pattern pattern = Pattern.compile(emailPattern);
+    public static boolean isValid(final String email){
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
     public Customer(String firstName, String lastName, String email){
         this.firstName = firstName;
         this.lastName = lastName;
-        if (email != "^(.+)@(.+).(.+)$"){
+        if (!isValid(email)){
             throw new IllegalArgumentException("This email is invalid");
         }
         this.email = email;
