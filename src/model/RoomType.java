93b4c4a56d29f467b03169d95a4be1c5cb51ad01
Ivie Osoiye <ivieosoiye@gmail.com>
2022-09-20 17:34:30 +0100

    added valueOflabel method

--- src/model/RoomType.java
+++ src/model/RoomType.java
@@ -1,6 +1,21 @@
 package model;
 
 public enum RoomType {
-    SINGLE,
-    DOUBLE
+    SINGLE("3"),
+    DOUBLE("2");
+
+    public final String label;
+
+    RoomType(String label) {
+        this.label = label;
+    }
+
+    public static RoomType valueOfLabel(String label) {
+        for (RoomType roomType : values()) {
+            if (roomType.label.equals(label)) {
+                return roomType;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
 }
