c205c4d1d28742030b2528da4d6421842b4cc094
Ivie Osoiye <ivieosoiye@gmail.com>
2022-09-20 15:10:19 +0100

    created the customer service class

---
+++ src/service/CustomerService.java
@@ -0,0 +1,19 @@
+package service;
+
+import model.Customer;
+
+import java.util.Collection;
+
+public class CustomerService {
+    public void addCustomer(String email, String firstName, String lastName){}
+    public Customer getCustomer(String customerEmail){
+        return null;
+    }
+    public Collection<Customer> getAllCustomers(){
+        return null;
+    }
+
+    static <T> void displayCustomer(T variable){
+        System.out.println(variable.getClass().getName());
+    }
+}
