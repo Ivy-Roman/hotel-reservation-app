07fbbc48e5ad15771757dcdb89c296ae0af4fa8c
Ivie Osoiye <ivieosoiye@gmail.com>
2022-09-20 16:36:03 +0100

    created the admin resource class

---
+++ src/api/AdminResource.java
@@ -0,0 +1,42 @@
+package api;
+
+import model.Customer;
+import model.IRoom;
+import service.CustomerService;
+import service.ReservationService;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AdminResource {
+    private static final AdminResource STANDARD = new AdminResource();
+
+    private final CustomerService customerService = CustomerService.getStandard();
+    private final ReservationService reservationService = ReservationService.getStandard();
+
+    private AdminResource() {}
+
+    public static AdminResource getStandard() {
+        return STANDARD;
+    }
+
+    public Customer getCustomer(String email) {
+        return customerService.getCustomer(email);
+    }
+
+    public void addRoom(List<IRoom> rooms) {
+        rooms.forEach(reservationService::addRoom);
+    }
+
+    public Collection<IRoom> getAllRooms() {
+        return reservationService.getAllRooms();
+    }
+
+    public Collection<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    public void displayAllReservations() {
+        reservationService.printAllReservation();
+    }
+}
