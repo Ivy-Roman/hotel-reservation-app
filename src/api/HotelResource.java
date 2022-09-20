3f8369ff8af1262922816b34c9c734ee6d924c13
Ivie Osoiye <ivieosoiye@gmail.com>
2022-09-20 16:17:13 +0100

    created the hotel resource class

---
+++ src/api/HotelResource.java
@@ -0,0 +1,54 @@
+package api;
+
+import model.Customer;
+import model.IRoom;
+import model.Reservation;
+import service.CustomerService;
+import service.ReservationService;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+
+public class HotelResource {
+    private static final HotelResource STANDARD = new HotelResource();
+
+    private final CustomerService customerService = CustomerService.getStandard();
+    private final ReservationService reservationService = ReservationService.getStandard();
+
+    private HotelResource(){};
+
+    public static HotelResource getStandard() {
+        return STANDARD;
+    }
+
+    public Customer getCustomer(String email) {
+        return customerService.getCustomer(email);
+    }
+
+    public void createACustomer(String email, String firstName, String lastName) {
+        customerService.addCustomer(email, firstName, lastName);
+    }
+
+    public IRoom getRoom(String roomNumber) {
+        return reservationService.getARoom(roomNumber);
+    }
+
+    public Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date checkOutDate) {
+        return reservationService.reserveARoom(getCustomer(customerEmail), room, checkInDate, checkOutDate);
+    }
+
+    public Collection<Reservation> getCustomersReservations(String customerEmail) {
+        final Customer customer = getCustomer(customerEmail);
+
+        if (customer == null) {
+            return Collections.emptyList();
+        }
+
+        return reservationService.getCustomersReservation(getCustomer(customerEmail));
+    }
+
+    public Collection<IRoom> findARoom(final Date checkIn, final Date checkOut) {
+        return reservationService.findRooms(checkIn, checkOut);
+    }
+}
